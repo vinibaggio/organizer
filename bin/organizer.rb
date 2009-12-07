d7abb798551e06d83014b42e68795b845ddaddae
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-07 00:50:12 -0200

    Starting to create the user interface using the choice library

--- bin/organizer.rb
+++ bin/organizer.rb
@@ -11,7 +11,35 @@
 
 # Copyright (c) 2009 Vinicius B. Fuentes http://me.vinibaggio.com,
 # released under the MIT license
+require 'rubygems'
+require 'choice'
 
-require File.expand_path(File.dirname(__FILE__) + "/../lib/organizer")
+# require File.expand_path(File.dirname(__FILE__) + "/../lib/organizer")
+
+class Organizer
+  class << self
+    def init
+      Choice.options do
+        header ''
+        header 'Options:'
+        
+        option :dryrun do
+          short '-d'
+          long '--dryrun'
+          desc 'Run the program normally but does not make any changes effective. Useful for testing.'
+        end
+        
+        option :rules do
+          short '-r'
+          long '--rules=RULES'
+          desc 'Specify the rules files for the program to read from.'
+        end
+      end
+    end
+  end
+end
+
+Organizer.init
+p Choice.choices.rules
 
 #         @rules_file = File.expand_path(File.dirname(__FILE__) + "/rules.yml")
